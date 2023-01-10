4bd35f48ef638d38a52f451d2e20bb1af3c722b7
GauravGoswami12967 <gaurav12967@gmail.com>
2023-01-10 13:58:56 +0530

    change

---
+++ Advance Javascript/React/myredux/src/app/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+//import userReducers from "../feature/users/userReducer";
+//import employeeReducer from "../feature/employeee/employeeReducer";
+
+const rootReducer=combineReducers({
+    //users: userReducers,
+    //emp:employeeReducer
+})
+
+export default rootReducer;
